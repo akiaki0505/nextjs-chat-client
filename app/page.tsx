f7f9ce9bcb46d05743a834b7a27ee8fad9ff3a86
akiaki0505 <85975247+akiaki0505@users.noreply.github.com>
2024-08-29 15:54:48 +0900

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,23 @@ import io from "socket.io-client";
 
 const socket = io("http://localhost:5000")
 
+
 export default function Home() {
   const [message, setMessage] = useState("");
+  const [list, setList] = useState([]);
 
   const handleSendMessage = () => {
     //serverへの送信
-    socket.emit("send_massage", {message: message});
+    socket.emit("send_message", { message: message });
     setMessage("");
   }
+
+  //サーバーから受信
+  socket.on("received_message", (data) => {
+    console.log(data);
+    setList([...list, data]);
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
@@ -24,6 +33,11 @@ export default function Home() {
         <div className="text-center">
           <button onClick={(() => handleSendMessage())}>チャット送信</button>
         </div>
+        {list.map((chat) => (
+          <div key={chat.message}>
+            {chat.message}
+          </div>
+        ))}
       </div>
     </main>
   );

c336863130e9a0dd95b050797633aef6c0b61f05
伊藤晃洋 <itoakihiro@itouakirayounoMacBook-Pro.local>
2024-09-17 21:50:52 +0900

    チャットエリアウィンドウの大きさによって自動でリサイズ

--- app/components/chat/chatList.jsx
+++ app/components/chat/chatList.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from "@/app/chat/styles/style.module.css";
 import { useChatData } from '@/app/features/hooks/useChatData';
 import { ChatMessages } from '@/app/components/chat/chatMessage';
@@ -8,9 +8,27 @@ import { ChatInput } from '@/app/components/chat/chatInput';
 export function ChatList() {
     const { ChatData, room_id, handleSendMessage, message, setMessage, user_name, textAreaRef } = useChatData();
 
+    const [windowHeight, setWindowHeight] = useState(0);
+
+    useEffect(() => {
+        // 初期ウィンドウサイズ設定
+        setWindowHeight(window.innerHeight);
+
+        // ウィンドウサイズ変更時に高さを更新
+        const handleResize = () => {
+            setWindowHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
   return (
     <div className={style.wrapper}>
-        <div className="p-4 sm:ml-64 overflow-y-scroll" style={{height:'35rem'}}>
+        <div className="p-4 sm:ml-64 overflow-y-scroll" style={{ height:`${windowHeight - 200}px`} }>
             <div className="p-4 border-gray-200 border-dashed rounded-lg">
                 <ChatMessages chatData={ChatData} />
             </div>
